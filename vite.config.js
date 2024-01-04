02e1124d95878f422e830fdec7f422853e698a0d
Wisanu Phornjariyatham <wisanu.pho@cru.ac.th>
2024-01-04 12:09:10 +0700

    defined base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://krustack.github.io/test-github-action-1/',
+  base: '/test-github-action-1/',
   plugins: [react()],
   test: {
     globals: true,
